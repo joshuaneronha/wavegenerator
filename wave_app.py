dff6b035d64e11f61beb4e375e1e464f4c336447
joshuaneronha <77899869+joshuaneronha@users.noreply.github.com>
2022-04-21 13:56:40 -0400

    0.0.1

--- wave_app.py
+++ wave_app.py
@@ -11,7 +11,7 @@ class WaveGeneratorApp:
     def __init__(self,win):
 
         window.title('Wave Generator Applet')
-        window.geometry("1000x1000+10+20")
+        window.geometry("800x400+10+20")
 
         self.title = Label(win, text = 'Wave Generator App', font = ("Arial",25))
         self.title.place(x = 180, anchor = "center", y=25)
@@ -25,12 +25,12 @@ class WaveGeneratorApp:
 
         self.freqfld=Entry(win, text="Frequency")
         self.freqfld.place(x=80, y=110, width = 80, anchor = "center")
-        self.freqlabel = Label(win, text = "Freq 1 (Hz)")
+        self.freqlabel = Label(win, text = "Right (Hz)")
         self.freqlabel.place(x=80, y=140, anchor = "center")
 
         self.freqfld1=Entry(win, text="Frequency1")
         self.freqfld1.place(x=180, y=110, width = 80, anchor = "center")
-        self.freqlabel1 = Label(win, text = "Freq 2 (Hz)")
+        self.freqlabel1 = Label(win, text = "Left(Hz)")
         self.freqlabel1.place(x=180, y=140, anchor = "center")
         self.optionallabel = Label(win, text = "--optional--")
         self.optionallabel.place(x=180, y=160, anchor = "center")
@@ -48,38 +48,42 @@ class WaveGeneratorApp:
         self.canvas.get_tk_widget().pack(side = tkinter.RIGHT,fill=tkinter.Y)
         NavigationToolbar2Tk(self.canvas, win)
 
-        #self.course = Label(win, text = 'LEFT')
-        #self.course.place(x = 180, anchor = "center", y=230)
-
-        self.fig1, self.ax1 = plt.subplots(figsize = (2,3))
-        self.canvas1 = FigureCanvasTkAgg(self.fig1, master = window)
-        self.canvas1.get_tk_widget().pack(side = tkinter.RIGHT,fill=tkinter.Y)
-        NavigationToolbar2Tk(self.canvas1, win)
-
 
     def generate_tone(self,win):
+
+        self.ax.clear()
+
         p = pyaudio.PyAudio()
 
         volume = 1     # range [0.0, 1.0]
         self.fs = 41000       # sampling rate, Hz, must be integer
 
+        try:
+            self.f = int(self.freqfld.get())      # sine frequency, Hz, may be float
+            self.f1 = int(self.freqfld1.get())      # sine frequency, Hz, may be float
+
+            self.flist = [self.f, self.f1]
+
+        except:
+            self.f = int(self.freqfld.get())      # sine frequency, Hz, may be float
+
+            self.flist = [self.f]
+
         if len(self.durfld.get()) != 0:
             self.duration = int(self.durfld.get())   # in seconds, may be float
 
             if len(self.freqfld.get()) != 0:
-                self.f = int(self.freqfld.get())      # sine frequency, Hz, may be float
                 self.time_array = np.arange(self.fs*self.duration)
                 self.wave = np.sin(2*np.pi*self.time_array*self.f/self.fs).astype(np.float32)
                 self.waveplot = np.sin(2*np.pi*self.time_array*self.f/self.fs).astype(np.float32)
-                self.plot_function(self.fig, self.ax, self.waveplot, self.time_array, self.canvas)
+                self.plot_function(self.fig, self.ax, self.waveplot, self.time_array, self.canvas,self.flist)
 
             if len(self.freqfld1.get()) != 0:
-                self.f1 = int(self.freqfld1.get())      # sine frequency, Hz, may be float
                 self.time_array1 = np.arange(self.fs*self.duration)
                 self.wave1 = np.sin(2*np.pi*self.time_array1*self.f1/self.fs).astype(np.float32)
                 self.waveplot1 = np.sin(2*np.pi*self.time_array1*self.f1/self.fs).astype(np.float32)
-                self.plot_function(self.fig1, self.ax1, self.waveplot1, self.time_array1, self.canvas1)
-                
+                self.plot_function(self.fig, self.ax, self.waveplot1, self.time_array1, self.canvas, self.flist)
+
             signal = 0
             channels = 1
 
@@ -112,12 +116,23 @@ class WaveGeneratorApp:
                 stream.close()
         p.terminate()
 
-    def plot_function(self, fig, ax, waveplot, time_array, canvas):
+    def plot_function(self, fig, ax, waveplot, time_array, canvas, flist):
 
         # fig, ax = plt.subplots(figsize = (2,1))
         # ax.set_xlim([0, 1000])
 
+        # fig.clf()
+
+        maxf = max(flist)
+
         ax.plot(time_array / self.fs, waveplot)
+
+        ax.set_xlim(0, 5/maxf)
+
+        ax.legend(['Right', 'Left'],loc = 'upper right')
+        ax.set_title('Frequency Plot')
+        ax.set_xlabel('Time')
+
         canvas.draw()
 
         # canvas = FigureCanvasTkAgg(fig, master = window)
